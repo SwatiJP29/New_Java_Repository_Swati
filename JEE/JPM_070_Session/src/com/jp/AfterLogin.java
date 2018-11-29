5c06058a750e3e6dd27408a7da4f5384b131c0c6
Administrator <swati.palkar@jpmorgan.com>
2018-11-29 13:08:24 +0530

    commit 1PM Thurs

--- JEE/JPM_070_Session/src/com/jp/AfterLogin.java
+++ JEE/JPM_070_Session/src/com/jp/AfterLogin.java
@@ -1,32 +1,34 @@
 package com.jp;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class AfterLogin
- */
+
 @WebServlet("/afterLogin")
 public class AfterLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//Create a session
+		HttpSession session = request.getSession(); //Create a session if does not exists.Hence one session per user
+		System.out.println("From after login : "+session.getId());
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher("/WEB-INF/jsps/MainMenu.jsp");
+		dispatch.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
---
+++ JEE/JPM_070_Session/src/com/jp/Logout.java
@@ -0,0 +1,32 @@
+package com.jp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		System.out.println("From logout "+ session.getId());
+		
+		//drop the session, space is removed and id is dropped
+		session.invalidate();
+		System.out.println("Session is droped");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
