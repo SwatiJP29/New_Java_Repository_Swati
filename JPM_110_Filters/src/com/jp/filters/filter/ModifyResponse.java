3eb8d04c03a91fc1262b29effdf6441bc6373624
Administrator <swati.palkar@jpmorgan.com>
2018-11-30 16:10:36 +0530

    Jee 4pm

--- JPM_110_Filters/src/com/jp/filters/filter/ModifyResponse.java
+++ JPM_110_Filters/src/com/jp/filters/filter/ModifyResponse.java
@@ -5,21 +5,28 @@ import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
 @WebFilter(dispatcherTypes = {DispatcherType.REQUEST }
-					, servletNames = { "Servlet1" }, urlPatterns={"/servlet1", "/servlet2"})
+					, urlPatterns={"*.do"})
 public class ModifyResponse implements Filter {
 
   
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		//Before part
 		System.out.println("In before part of filter");
+		RequestDispatcher dispatch= request.getRequestDispatcher("/WEB-INF/jsps/Head.jsp");
+		dispatch.include(request, response);
+		
 		chain.doFilter(request, response); // this will send the control to the servlet, service method of the servlet would be execute and the control will come back here
+		
 		//After part
+		dispatch= request.getRequestDispatcher("/WEB-INF/jsps/Footer.jsp");
+		dispatch.include(request, response);
 		System.out.println("In after part of filter");
 	}
 	
--- JPM_110_Filters/src/com/jp/filters/servlet/Servlet1.java
+++ JPM_110_Filters/src/com/jp/filters/servlet/Servlet1.java
@@ -1,11 +1,15 @@
 package com.jp.filters.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 
 
 @WebServlet(urlPatterns="/servlet1", loadOnStartup=1)
@@ -14,6 +18,8 @@ public class Servlet1 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("In Servlet");
+		RequestDispatcher dispatch = request.getRequestDispatcher("/WEB-INF/jsps/MyJSP1.jsp");
+		dispatch.include(request, response);
 	}
 
 	
